310838355a5ccc811a7f9fb732e55cdb21dc4ca6
Pedro Esteves <kropinho211@outlook.com>
2024-05-21 11:20:05 -0300

    Desafio ToDo list

---
+++ desafio-todolist/src/main/java/com/kropsz/github/desafiotodolist/DesafioTodolistApplication.java
@@ -0,0 +1,14 @@
+package com.kropsz.github.desafiotodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodolistApplication {
+
+	public static void main(String[] args) {
+	SpringApplication.run(DesafioTodolistApplication.class, args);
+	}
+
+}
+
---
+++ desafio-todolist/src/test/java/com/kropsz/github/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kropsz.github.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
